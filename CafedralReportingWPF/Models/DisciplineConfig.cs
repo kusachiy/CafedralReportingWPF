2fcaf1828af7dc264799c6b925aea4a149161a5a
Alexander Savelyev <xavierernandez@gmail.com>
2020-05-24 20:58:36 +0300

    Added config page (2)

---
+++ CafedralReportingWPF/Models/DisciplineConfig.cs
@@ -0,0 +1,24 @@
+﻿using CafedralReportingWPF.Models.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafedralReportingWPF.Models
+{
+    [Table("DisciplineConfig")]
+    public class DisciplineConfig: BaseModel
+    {
+        public int EmployeeId { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+
+        public int DisciplineId { get; set; }
+        [ForeignKey("DisciplineId")]
+        public Discipline Discipline { get; set; }
+
+        public int? Agreement { get; set; }
+    }
+}
---
+++ CafedralReportingWPF/Views/DisciplineConfigPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using CafedralReportingWPF.DataSource;
+using CafedralReportingWPF.DataSource.DbWorkers;
+using CafedralReportingWPF.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CafedralReportingWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for WorkflowControl.xaml
+    /// </summary>
+    public partial class DisciplineConfigPage : Page
+    {
+        private Context _context; 
+        public DisciplineConfigPage()
+        {
+            InitializeComponent();
+            _context = DbContextSingleton.GetContext();
+           _context.DisciplineConfig.Include(d=>d.Discipline).Include(d=>d.Employee).Load();
+            this.DataContext = _context.DisciplineConfig.Local.ToBindingList();
+        }
+              
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _context.SaveChanges();
+        }
+    }
+}
